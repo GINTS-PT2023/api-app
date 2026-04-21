6ff6e33a3efc6cdb4d36e8e8fa33cbc95125ae30
Girgensons <ballergints@gmail.com>
2026-04-21 11:39:50 +0300

    create post method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,16 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
 
     public function index(){
         return Post::all();
+        // return "index";
     }
 
     public function store(Request $request){
-        
+        $fields = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ]);
+
+        // $post = $request->create($fields);
+        $post = Post::create($fields);
+
+        return['post' => $post];
     }
 
     public function update(Request $request){
